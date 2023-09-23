7e8edf9595df680aabd9d40582263bcbf7d5a74c
JhoneiroLove <jhoneiro12@hotmail.com>
2023-09-23 17:06:07 -0500

    Jhoel Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JhoelComponent } from './shared/component/jhoel/jhoel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JhoelComponent
   ],
   imports: [
     BrowserModule,
